09efac32fb3a6258c1e03de67a5d842f1b4a3b3f
NehaKamath29 <nehabhaskark.cs21@bmsce.ac.in>
2023-08-28 02:13:24 +0530

    Final

--- Lab3-RoundRobin.c
+++ Lab3-RoundRobin.c
@@ -1,52 +1,65 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
 
-  int cnt,j,n,t,remain,flag=0,tq;
-  int wt=0,tat=0,at[10],bt[10],rt[10];
-  printf("Enter Total Process:\t ");
-  scanf("%d",&n);
-  remain=n;
-  for(cnt=0;cnt<n;cnt++)
-  {
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",cnt+1);
-    scanf("%d",&at[cnt]);
-    scanf("%d",&bt[cnt]);
-    rt[cnt]=bt[cnt];
-  }
-  printf("Enter Time Quantum:\t");
-  scanf("%d",&tq);
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
-  for(t=0,cnt=0;remain!=0;)
-  {
-    if(rt[cnt]<=tq && rt[cnt]>0)
-    {
-      t+=rt[cnt];
-      rt[cnt]=0;
-      flag=1;
-    }
-    else if(rt[cnt]>0)
-    {
-      rt[cnt]-=tq;
-      t+=tq;
-    }
-    if(rt[cnt]==0 && flag==1)
-    {
-      remain--;
-      printf("P[%d]\t|\t%d\t|\t%d\n",cnt+1,t-at[cnt],t-at[cnt]-bt[cnt]);
-      wt+=t-at[cnt]-bt[cnt];
-      tat+=t-at[cnt];
-      flag=0;
-    }
-    if(cnt==n-1)
-      cnt=0;
-    else if(at[cnt+1]<=t)
-      cnt++;
-    else
-      cnt=0;
-  }
-  printf("\nAverage Waiting Time= %f\n",wt*1.0/n);
-  printf("Avg Turnaround Time = %f",tat*1.0/n);
+#define MAX_PROCESSES 4
+#define TIME_QUANTUM 2
 
-  return 0;
+void roundRobinScheduling(int processes[], int burst_times[], int arrival_times[], int n) {
+    int remaining_time[MAX_PROCESSES];
+    int completion_time[MAX_PROCESSES];
+    int turnaround_times[MAX_PROCESSES];
+    int waiting_times[MAX_PROCESSES];
+
+    for (int i = 0; i < n; i++) {
+        remaining_time[i] = burst_times[i];
+        completion_time[i] = 0;
+    }
+
+    int current_time = 0;
+    int completed_processes = 0;
+
+    while (completed_processes < n) {
+        for (int i = 0; i < n; i++) {
+            if (arrival_times[i] <= current_time && remaining_time[i] > 0) {
+                int execute_time = (remaining_time[i] > TIME_QUANTUM) ? TIME_QUANTUM : remaining_time[i];
+                current_time += execute_time;
+                remaining_time[i] -= execute_time;
+
+                if (remaining_time[i] == 0) {
+                    completion_time[i] = current_time;
+                    completed_processes++;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        turnaround_times[i] = completion_time[i] - arrival_times[i];
+        waiting_times[i] = turnaround_times[i] - burst_times[i];
+    }
+
+    double avg_turnaround_time = 0, avg_waiting_time = 0;
+    for (int i = 0; i < n; i++) {
+        avg_turnaround_time += turnaround_times[i];
+        avg_waiting_time += waiting_times[i];
+    }
+    avg_turnaround_time /= n;
+    avg_waiting_time /= n;
+
+    printf("Process\tTurnaround Time\tWaiting Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\n", processes[i], turnaround_times[i], waiting_times[i]);
+    }
+    printf("\nAverage Turnaround Time: %.2lf\n", avg_turnaround_time);
+    printf("Average Waiting Time: %.2lf\n", avg_waiting_time);
+}
+
+int main() {
+    int processes[] = {1, 2, 3, 4};
+    int burst_times[] = {4,7,2,2};
+    int arrival_times[] = {0,2,3,3};
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    roundRobinScheduling(processes, burst_times, arrival_times, n);
+
+    return 0;
 }
